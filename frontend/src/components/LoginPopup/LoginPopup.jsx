584cd9ae1a0af222f6d9ac19021efa247850c175
aski anaan <askianaan007@gmail.com>
2024-08-06 13:27:55 +0530

    frontend

---
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./LoginPopup.css";
+
+const LoginPopup = () => {
+  return <div className="login-popup">
+    <form action="" className="login-popup-container"></form>
+     </div>;
+};
+
+export default LoginPopup;
